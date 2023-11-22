1a244178156a87e5fab61c053c0c677564757f74
AbhijeetKokat <abhijeetkokat2003@gmail.com>
2023-11-22 22:26:16 +0530

    added css

--- src/views/Home/Home.js
+++ src/views/Home/Home.js
@@ -7,13 +7,14 @@ function Home() {
     // const [lang, setLang] = useState("mr");
     const userCount = 30;
   return (
-    <div>
+    <div> 
+       <h1 className='nav-container'>Internationalization</h1>
+    <div className='i18n-container'>
       
-      <h1>{I18n("welcomeMessage")}</h1>
-      <p>{I18n("normalMessage")}</p>
-      <h3>{I18n("greetingMessage")}</h3>
-
-      <select value={localStorage.getItem("language")}
+      
+     <div className='text-center'>
+      <p>Select Language</p>
+     <select className='drop-lan'  value={localStorage.getItem("language")}
       onChange={(e) => {
         localStorage.setItem("language", e.target.value);
         window.location.reload();
@@ -23,12 +24,26 @@ function Home() {
         <option value="hi">Hindi</option>
         <option value="en">English</option>
       </select>
+     </div>
+
+      {/* <h1>{I18n("welcomeMessage")}</h1> */}
+      <h1 className='hed-1'>{I18n("normalMessage")}</h1>
+      <h3 className='queation-1'>{I18n("greetingMessage")}</h3>
+      <p className='ans-1'>{I18n("ans-1")}</p>
+
+      <h3 className='queation-1' >{I18n("question-2")}</h3>
+      <p className='ans-1'>{I18n("ans-2")}</p>
+
+      <h3 className='queation-1'>{I18n("question-3")}</h3>
+      <p className='ans-1'>{I18n("ans-3")}</p>
+
 
       <p>
-        {I18n("userStartMessage", "<studentCount>", userCount)}
+        {/* {I18n("userStartMessage", "<studentCount>", userCount)} */}
       </p>
+    </div>
     </div>
   )
 }
 
-export default Home
+export default Home;
